a785b2e0a11943a6de28771b56682d0a19b2640d
jenhsiang <chentai008@yahoo.com>
2014-08-09 11:04:46 +0800

    test
    Signed-off-by: jenhsiang <chentai008@yahoo.com>

---
+++ js/model.js
@@ -0,0 +1,27 @@
+﻿var viewModel = {
+	firstName:ko.observable("init")
+	,lastName:ko.observable("init")
+	,capitalizeLastName:function(){
+		var currentVal = this.lastName();
+		this.lastName(currentVal.toUpperCase());
+	}	
+};
+var add = function (){
+	return parseInt(this.firstName(),10)  + parseInt(this.lastName(),10);
+}
+var subtract =function(){
+	return parseInt(this.firstName(),10)  - parseInt(this.lastName(),10);
+}
+var historical =function (){
+	return parseInt(this.firstName(),10)  * parseInt(this.lastName(),10);
+}
+var rid =function (){
+	return parseInt(this.firstName(),10)  / parseInt(this.lastName(),10);
+}
+
+viewModel.fullName = ko.computed(function(){
+	return this.firstName() + "	" + this.lastName();
+},viewModel);
+//console.log(viewModel.firstName);
+//var ar =[];
+ko.applyBindings(viewModel);
